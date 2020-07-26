c0122f1c45b25842e2e1584a80651026cd4995b9
Geta Bugnar <bugnargeta@gmail.com>
2020-07-26 14:48:10 +0300

    added CRUD methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,23 @@
 package org.fasttrackit;
 
+import org.fasttrackit.persistance.TaskRepository;
+import org.fasttrackit.transfer.UpdateTaskRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException, SQLException {
+
+        TaskRepository taskRepository = new TaskRepository();
+
+        UpdateTaskRequest request = new UpdateTaskRequest();
+        request.setDone(true);
+
+        taskRepository.updateTask(2, request);
+        taskRepository.deleteTask(3);
     }
 }
